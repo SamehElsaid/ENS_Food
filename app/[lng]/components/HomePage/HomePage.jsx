3f44cefff0c49ca24c7b651956ddce9529f8280f
SamehElmeligy <samehmeligy3629@gmail.com>
2023-06-20 20:33:43 +0300

    Add Skeleton

--- app/[lng]/components/HomePage/HomePage.jsx
+++ app/[lng]/components/HomePage/HomePage.jsx
@@ -2,17 +2,27 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
+import Skeleton from "../Skeleton/Skeleton";
 function HomePage({ data, lang, title, seeMore }) {
   const containerRef = useRef(null);
   const [divId, setDivId] = useState(0);
   const headerRef = useRef(null);
-  const [mouseDown, setMouseDown] = useState(false);
-  const [initialMouseX, setInitialMouseX] = useState(0);
-  const [initialMouseY, setInitialMouseY] = useState(0);
-  const [initialScrollLeft, setInitialScrollLeft] = useState(0);
-  const [initialScrollTop, setInitialScrollTop] = useState(0);
   const [sortedHeader, setShortHead] = useState([...data.data]);
+  const swiperRef = useRef(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      if (divId !== 0) {
+        const find = sortedHeader.find((e) => e.id === divId);
+        const index = sortedHeader.findIndex((head) => head.id === divId);
+        const slicedBefore = sortedHeader.slice(0, index);
+        const slicedAfter = sortedHeader.slice(index + 1);
+        setShortHead([find, ...slicedAfter, ...slicedBefore]);
+      }
+    }, 500);
+  }, [divId]);
   useEffect(() => {
     const handleScroll = () => {
       const container = containerRef.current;
@@ -33,7 +43,6 @@ function HomePage({ data, lang, title, seeMore }) {
         }
       });
     };
-
     const container = containerRef.current;
     container.addEventListener("scroll", handleScroll);
     return () => {
@@ -41,68 +50,6 @@ function HomePage({ data, lang, title, seeMore }) {
     };
   }, []);
 
-  const handleMouseDown = (event) => {
-    setMouseDown(true);
-    setInitialMouseX(event.clientX);
-    setInitialScrollLeft(headerRef.current.scrollLeft);
-  };
-
-  const handleMouseUp = () => {
-    setMouseDown(false);
-  };
-
-  const handleMouseMove = (event) => {
-    if (!mouseDown) return;
-
-    const deltaX = event.clientX - initialMouseX;
-    const deltaY = event.clientY - initialMouseY;
-    const newScrollLeft = initialScrollLeft - deltaX;
-    const newScrollTop = initialScrollTop - deltaY;
-
-    // Limit the horizontal scroll position
-    const maxScrollLeft =
-      headerRef.current.scrollWidth - headerRef.current.clientWidth;
-    const limitedScrollLeft = Math.min(
-      Math.max(newScrollLeft, 0),
-      maxScrollLeft
-    );
-
-    // Limit the vertical scroll position
-    const maxScrollTop =
-      headerRef.current.scrollHeight - headerRef.current.clientHeight;
-    const limitedScrollTop = Math.min(Math.max(newScrollTop, 0), maxScrollTop);
-
-    headerRef.current.scrollLeft = limitedScrollLeft;
-    headerRef.current.scrollTop = limitedScrollTop;
-  };
-  useEffect(() => {
-    const header = headerRef.current;
-
-    const handleDocumentMouseUp = () => {
-      setMouseDown(false);
-    };
-
-    header.addEventListener("mouseup", handleMouseUp);
-    header.addEventListener("mousemove", handleMouseMove);
-    document.addEventListener("mouseup", handleDocumentMouseUp);
-
-    return () => {
-      header.removeEventListener("mouseup", handleMouseUp);
-      header.removeEventListener("mousemove", handleMouseMove);
-      document.removeEventListener("mouseup", handleDocumentMouseUp);
-    };
-  }, [mouseDown]);
-
-  useEffect(() => {
-    if (divId !== 0) {
-      const find = sortedHeader.find((e) => e.id === divId);
-      const index = sortedHeader.findIndex((head) => head.id === divId);
-      const slicedBefore = sortedHeader.slice(0, index);
-      const slicedAfter = sortedHeader.slice(index + 1);
-      setShortHead([find, ...slicedAfter, ...slicedBefore]);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [divId]);
   const handleClick = (id) => {
     const targetDiv = document.getElementById(id);
     if (targetDiv) {
@@ -113,52 +60,100 @@ function HomePage({ data, lang, title, seeMore }) {
         behavior: "smooth",
       });
     }
+    if (swiperRef.current) {
+      swiperRef.current.slideTo(0); // Scroll back to the initial slide (index 0)
+    }
   };
+  useEffect(() => {
+    setLoading(false);
+  }, []);
   return (
     <div
       ref={containerRef}
       className="overflow-y-auto || scrollStyle || min-h-[300px] || h-screen || relative "
     >
-      <h2 className="border-b-[3px] || border-mainColor || h-[50px] || flex || items-center || justify-center">
+      <h2 className="border-b-[3px] || border-mainColor || h-[50px] || flex || items-center || justify-center || mx-4">
         <span className="font-semibold">{title}</span>
       </h2>
-
-      <header
+      <div
         ref={headerRef}
-        onMouseDown={handleMouseDown}
-        className="flex  || select-none || w-full || scrollContainer  || overflow-x-auto || gap-2 || items-center || justify-between || box-shadow-edit || pt-3.5 || px-4 || sticky || top-[-1px] || z-10 || bg-white"
+        className="py-5 || box-shadow-edit || sticky || top-[-1px] || z-20 || bg-white ps-4 "
       >
-        {sortedHeader.map((category, i) => (
-          <div
-            onClick={() => handleClick(category.id)}
-            className={`
-            ${
-              divId !== 0
-                ? divId === category.id
-                  ? "bg-mainColor || text-white"
-                  : "bg-mainText"
-                : ""
-            }
-            ${divId === 0 && i === 0 ? `bg-mainColor || text-white` : ``} 
+        <div className="w-full">
+        <Swiper
+          className={`w-full ${
+            loading
+              ? "opacity-0"
+              : "opacity-100 || transition-opacity || duration-300"
+          }`}
+          onSwiper={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+          resizeReInit={true}
+          spaceBetween={5}
+          slidesPerView={"auto"}
+        //   breakpoints={{
+        //     0: {
+        //       slidesPerView: 3,
+        //     },
+        //     640: {
+        //       slidesPerView: 3,
+        //     },
+        //     768: {
+        //       slidesPerView: 3,
+        //     },
+        //     1024: {
+        //       slidesPerView: 3,
+        //     },
+        //     1280: {
+        //       slidesPerView: 3,
+        //     },
+        //   }}
+        >
+          {sortedHeader.map((category, i) => (
+            <SwiperSlide key={i} onClick={() => handleClick(category.id)}>
+              <div
+                className={`
+            ${i === 0 ? "bg-mainColor || text-white" : "bg-mainText"}
             
-            px-3 || header || min-w-fit || whitespace-nowrap || py-2  || text-sm || rounded-full || cursor-pointer`}
-            key={i}
-          >
-            {category[`${lang}_name`]}.
-          </div>
-        ))}
-      </header>
+            px-3 || header || min-w-fit  || py-2  || text-sm || rounded-full || cursor-pointer`}
+                key={i}
+              >
+                {category[`${lang}_name`]}.
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        </div>
+      </div>
       {data &&
         data.data.map((category, i) => (
-          <div className="head || mx-4" id={category.id} key={category.id}>
-            <h2 className="text-[#392200] || mt-8 || mb-4 || text-[32px] || font-bold">
-              {category[`${lang}_name`]}
+          <div className="head || mx-4 |" id={category.id} key={category.id}>
+            <h2 className="text-[#392200] || relative || mt-8 || mb-4 || text-[32px] || font-bold">
+              <div
+                className={`${
+                  !loading
+                    ? "opacity-0"
+                    : "opacity-100 || transition-opacity || duration-300  || animate-pulse"
+                } absolute || w-full || top-0 || h-full || bg-gray-300`}
+              ></div>
+              <span
+                className={`${
+                  loading
+                    ? "opacity-0"
+                    : "opacity-100 || transition-opacity || duration-300"
+                }`}
+              >
+                {" "}
+                {category[`${lang}_name`]}
+              </span>
             </h2>
             {data.meal[i].data.map((meal) => (
               <div
                 className="pb-[35px] || relative  || border-b || border-[#e0e0e0]"
                 key={meal.id}
               >
+                <Skeleton loading={loading} />
                 <Link
                   href={`${lang}/product/${category.id}/${meal.id}`}
                   className="flex || gap-2 || items-center"
---
+++ app/[lng]/components/Skeleton/Skeleton.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Skeleton({ loading }) {
+  return (
+    <div
+      className={`
+                ${
+                  !loading
+                    ? "opacity-0"
+                    : "opacity-100 || transition-opacity || duration-300"
+                } || skeleton || absolute || top-1/2 || left-1/2 || -translate-x-1/2 || -translate-y-1/2 || z-10`}
+    >
+      <div className="skeleton-left">
+        <div className="line h17 w40 m10"></div>
+        <div className="line"></div>
+        <div className="line h8 w50"></div>
+        <div className="line  w75"></div>
+      </div>
+      <div className="skeleton-right">
+        <div className="square"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Skeleton;
