710e9509010d2a534f8f4df677f9cee033fda4b1
SamehElmeligy <samehmeligy3629@gmail.com>
2023-06-22 04:56:14 +0300

    Create NextTopLoader To load Page

--- app/[lng]/layout.jsx
+++ app/[lng]/layout.jsx
@@ -1,8 +1,10 @@
 import { dir } from "i18next";
 import { languages } from "../i18n/settings";
-import "./../globals.css"
+import "./../globals.css";
 import Providers from "./components/Global/Providers";
 import { useTranslation } from "../i18n";
+import NextTopLoader from "nextjs-toploader";
+
 export async function generateStaticParams() {
   return languages.map((lng) => ({ lng }));
 }
@@ -16,7 +18,20 @@ export default async function RootLayout({ children, params: { lng } }) {
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body suppressHydrationWarning={true}>
-        <Providers lng={i18n.resolvedLanguage}>{children}</Providers>
+        <Providers lng={i18n.resolvedLanguage}>
+          <NextTopLoader
+            color="#b53131"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl={true}
+            showSpinner={true}
+            easing="linear"
+            speed={200}
+            shadow={false}
+          />
+          {children}
+        </Providers>
       </body>
     </html>
   );
